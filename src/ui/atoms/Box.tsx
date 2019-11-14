1d577e69363e8e39b8f5be60be73722a0256e831
Anton Kosykh <kelin2025@yandex.ru>
2019-11-14 03:43:02 +0300

    Improved Box (also switch back to SC)

--- src/ui/atoms/Box.tsx
+++ src/ui/atoms/Box.tsx
@@ -1,55 +1,73 @@
-import * as React from "react"
-import { styled } from "linaria/react"
+import * as React from "react";
+import styled from "styled-components";
 
 type BoxT = React.FunctionComponent<{
-  align?: "start" | "center" | "end"
-  inline?: boolean
-  vertical?: boolean
-  cols?: number | string | number[] | string[]
-  rows?: number | string | number[] | string[]
-  gap?: number | string
-  justify?: "start" | "center" | "end" | "space-between"
-  padding?: number | string
-  className?: string
-}>
+  align?: "start" | "center" | "end";
+  inline?: boolean;
+  vertical?: boolean;
+  width?: number | string;
+  cols?: number | string | number[] | string[];
+  rows?: number | string | number[] | string[];
+  gap?: number | string;
+  justify?: "start" | "center" | "end" | "space-between";
+  padding?: number | string;
+  className?: string;
+}>;
 
-const align = props => props.align || "center"
+const align = props => props.align || "center";
 
-const display = props => (props.inline ? "inline-grid" : "grid")
+const display = props => (props.inline ? "inline-grid" : "grid");
 
-const flow = props => props.flow || "initial"
+const flow = props => props.flow || "initial";
+
+const width = props => props.width || "initial";
 
 const cols = props =>
   props.cols
     ? Array.isArray(props.cols)
       ? props.cols.join(" ")
-      : `repeat(${props.cols}, 1fr)`
-    : `auto`
+      : typeof props.cols === "number"
+      ? `repeat(${props.cols}, 1fr)`
+      : props.cols
+    : `auto`;
 
 const rows = props =>
   props.rows
     ? Array.isArray(props.rows)
       ? props.rows.join(" ")
       : `repeat(${props.rows}, 1fr)`
-    : `auto`
+    : `auto`;
 
 const gap = props =>
   "gap" in props
     ? typeof props.gap === "string"
       ? props.gap
       : `${props.gap}px`
-    : `10px`
+    : `10px`;
 
-const justify = props => props.justify || "start"
+const justify = props => props.justify || "start";
 
 const padding = props =>
   "padding" in props
     ? typeof props.padding === "string"
       ? props.padding
       : `${props.padding}px`
-    : `0px`
+    : `0px`;
 
-export const Box: BoxT = styled.div`
+const BoxView: BoxT = React.forwardRef(
+  ({ tag = "div", className, children, ...props }, ref) => {
+    const Tag = tag;
+
+    return (
+      <Tag ref={ref} className={className} {...props}>
+        {children}
+      </Tag>
+    );
+  }
+);
+
+export const Box = styled(BoxView)`
+  width: ${width};
   align-items: ${align};
   display: ${display};
   grid-auto-flow: ${flow};
@@ -58,4 +76,4 @@ export const Box: BoxT = styled.div`
   grid-gap: ${gap};
   justify-content: ${justify};
   padding: ${padding};
-`
+`;
